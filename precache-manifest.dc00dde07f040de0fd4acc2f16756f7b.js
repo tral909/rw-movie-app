2130e878f506692882fefa19a61245be40d18961
Egorov Roman <r.egorov@vbankcenter.ru>
2020-04-14 16:36:09 +0300

    Updates

---
+++ precache-manifest.dc00dde07f040de0fd4acc2f16756f7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8517046d2b3c884c56001f8904183239",
+    "url": "/rw-movie-app/index.html"
+  },
+  {
+    "revision": "8d4fd5d6b9ec30168e52",
+    "url": "/rw-movie-app/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "74214c7e328f7da0aaf7",
+    "url": "/rw-movie-app/static/css/main.79e7f76d.chunk.css"
+  },
+  {
+    "revision": "8d4fd5d6b9ec30168e52",
+    "url": "/rw-movie-app/static/js/2.3e9b1fc8.chunk.js"
+  },
+  {
+    "revision": "74214c7e328f7da0aaf7",
+    "url": "/rw-movie-app/static/js/main.ccc468dc.chunk.js"
+  },
+  {
+    "revision": "10fa88b87fcdb5a67c5f",
+    "url": "/rw-movie-app/static/js/runtime~main.06f679bd.js"
+  }
+]);
